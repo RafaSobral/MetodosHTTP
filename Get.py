4c09e2620405636ab9d287910e3b646e1bacf2af
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-01-19 22:01:23 -0300

    Outros metodos

--- Get.py
+++ Get.py
@@ -1,7 +1,7 @@
 '''Importa a biblioteca necessaria para fazer requisicoes'''
 import requests
 '''Faz a requisicao no link da API'''
-requisicao = requests.get("LINK DA API")
+requisicao = requests.get("https://metodoshttp-198c0-default-rtdb.europe-west1.firebasedatabase.app/.json")
 '''Printa a resposta da API para a requisicao. 200 = Sucess, 404 = Failed'''
 print(requisicao)
 '''printa o dicionario JSON retornado pela API'''
